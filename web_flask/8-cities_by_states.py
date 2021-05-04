350ff1f243f875a861eaf3f45768874fd2f7412a
aydentownsley <aydentownsley@gmail.com>
2021-05-04 02:53:01 +0000

    adding test

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,22 +9,18 @@ app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def storage_close(context):
-    """[summary]
-    """
+def teardown(context):
+    '''Method to remove current SQLAlchemy Session'''
     storage.close()
 
 
-@app.route('/cities_by_states')
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_route():
-    """[summary]
+    '''Method to load all cities of a State'''
 
-    Returns:
-        [type]: [description]
-    """
     stateList = storage.all(State)
 
-    return render_template('8-cities_by_states.html', all_states=all_states)
+    return render_template('8-cities_by_states.html', stateList=stateList)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
